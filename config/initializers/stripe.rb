345cd33f9e0add6c85d185b8a21e415f0347a966
rajan-gith <rajan.prajapati@berylsystems.com>
2019-09-04 18:29:45 +0530

    minor updates

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.credentials.stripe[Rails.env.to_sym][:publishable_key],
+  secret_key:      Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
