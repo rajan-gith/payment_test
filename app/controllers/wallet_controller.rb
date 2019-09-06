81b0c23c7f2f56678507462e8679a43d52aea4cb
rajan-gith <rajan.prajapati@berylsystems.com>
2019-09-06 18:06:01 +0530

    wallet controller added

---
+++ app/controllers/wallet_controller.rb
@@ -0,0 +1,25 @@
+class WalletController < ApplicationController
+  before_action :authenticate_user!
+  rescue_from Stripe::CardError, with: :catch_exception
+  def index
+  end
+
+  def new
+  end
+
+  def new_card
+  end
+
+  def add
+    @wallet_amount = params[:user][:wallet_amount]
+    StripeChargesServices.new(params[:stripeToken], current_user.email, current_user, @wallet_amount).call
+  end
+  def charges_params
+    params.permit(:stripeEmail, :stripeToken, :order_id)
+  end
+
+  def catch_exception(exception)
+    flash[:error] = exception.message
+  end
+
+end
